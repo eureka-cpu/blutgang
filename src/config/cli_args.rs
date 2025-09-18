a2baf542bfb8f07c7e72655998de3daff29312ec
eureka-cpu <github.eureka@gmail.com>
2025-09-18 00:35:35 -0700

    initial cli refactor, need to understand parsed config options

--- src/config/cli_args.rs
+++ src/config/cli_args.rs
@@ -1,8 +1,9 @@
+use std::path::PathBuf;
+
 use crate::config::system::VERSION_STR;
 
 use clap::{
-    Arg,
-    Command,
+    CommandFactory,
     Parser,
 };
 
@@ -14,123 +15,96 @@ use clap::{
     about = "Blutgang load balancer and cache. For more info read the wiki: https://github.com/rainshowerLabs/blutgang/wiki",
 )]
 pub struct Blutgang {
-    // TODO: @eureka-cpu -- create FromStr impls or use value_parser. In some cases from_args_matches may work better,
-    // see if there is a work around to make the interface simpler.
-    //
-    // #[arg(long, short = 'r', num_args = 1.., conflicts_with = "config", help = "CSV list of rpcs")]
-    // rpc_list: Vec<crate::rpc::types::Rpc>,
+    /// TOML config file for blutgang.
+    #[arg(
+        long,
+        short = 'c',
+        default_value = "config.toml",
+        conflicts_with = "rpc_list"
+    )]
+    config: PathBuf,
+
+    /// List of RPCs in CSV format (comma separated values).
+    #[arg(long, short = 'r', conflicts_with = "config")]
+    rpc_list: Vec<String>,
+
+    /// Latency moving average length.
+    #[arg(long, default_value_t = 15)]
+    ma_length: usize,
+
+    /// Address to listen to.
+    #[arg(long, short = 'a', default_value = "127.0.0.1")]
+    address: String,
+
+    /// Port to listen to.
+    #[arg(long, short = 'p', default_value_t = 3000)]
+    port: u16,
+
+    /// Database path.
+    #[arg(long, short = 'd', default_value = "blutgang-cache")]
+    db: PathBuf,
+
+    /// Capacity of the cache stored in memory in bytes.
+    #[arg(long, default_value_t = 1000000000)]
+    cache_capacity: u64,
+
+    /// Zstd compression level.
+    #[arg(long)]
+    compression: String,
+
+    /// Time in ms to flush the DB.
+    #[arg(long, default_value_t = 1000)]
+    flush_every_ms: u32,
+
+    /// Clear cache.
+    #[arg(long, default_value_t = false)]
+    clear: bool,
+
+    /// Enable health checking.
+    #[arg(long, default_value_t = false)]
+    health_check: bool,
+
+    /// How often to perform the health check.
+    #[arg(long, default_value_t = 2000)]
+    health_check_ttl: u32,
+
+    /// Time for the RPC to respond before we remove it from the active queue.
+    #[arg(long, default_value_t = 300)]
+    ttl: u32,
+
+    /// Supress the checking RPC health messages.
+    #[arg(long, default_value_t = false)]
+    supress_rpc_check: bool,
+
+    /// Maximum amount of retries before we drop the current request.
+    #[arg(long, default_value_t = 32)]
+    max_retries: u64,
+
+    /// Enable the admin namespace.
+    #[arg(long, default_value_t = false)]
+    admin: bool,
+
+    /// Address to listen to for the admin namespace.
+    #[arg(long, default_value = "127.0.0.1")]
+    admin_address: String,
+
+    /// Port to listen to for the admin namespace.
+    #[arg(long, default_value_t = 5715)]
+    admin_port: u16,
+
+    /// Make the admin namespace be readonly.
+    #[arg(long, default_value_t = false)]
+    readonly: bool,
+
+    /// Enable admin comms with JWT.
+    #[arg(long, requires = "token")]
+    jwt: String,
+
+    /// JWT token.
+    #[arg(long, requires = "jwt")]
+    token: String,
 }
 
 pub fn create_match() -> clap::Command {
-    Command::new("blutgang")
-        .version(VERSION_STR)
-        .author("makemake <vukasin@gostovic.me> and contributors")
-        .about("Blutgang load balancer and cache. For more info read the wiki: https://github.com/rainshowerLabs/blutgang/wiki")
-        .arg(Arg::new("rpc_list")
-            .long("rpc_list")
-            .short('r')
-            .num_args(1..)
-            .default_value("")
-            .conflicts_with("config")
-            .help("CSV list of rpcs"))
-        .arg(Arg::new("config")
-            .long("config")
-            .short('c')
-            .num_args(1..)
-            .default_value("config.toml")
-            .conflicts_with("rpc_list")
-            .help("TOML config file for blutgang"))
-        .arg(Arg::new("port")
-            .long("port")
-            .short('p')
-            .num_args(1..)
-            .default_value("3000")
-            .help("port to listen to"))
-        .arg(Arg::new("address")
-            .long("address")
-            .short('a')
-            .num_args(1..)
-            .default_value("127.0.0.1")
-            .help("port to listen to"))
-        .arg(Arg::new("ma_length")
-            .long("ma_length")
-            .num_args(1..)
-            .default_value("15")
-            .help("Latency moving average length"))
-        .arg(Arg::new("db")
-            .long("db")
-            .short('d')
-            .num_args(1..)
-            .default_value("blutgang-cache")
-            .help("Database path"))
-        .arg(Arg::new("cache_capacity")
-            .long("cache_capacity")
-            .num_args(1..)
-            .default_value("1000000000")
-            .help("Capacity of the cache stored in memory in bytes"))
-        .arg(Arg::new("compression")
-            .long("compression")
-            .num_args(0..)
-            .help("Zstd compression level"))
-        .arg(Arg::new("flush_every_ms")
-            .long("flush_every_ms")
-            .num_args(1..)
-            .default_value("1000")
-            .help("Time in ms to flush the DB"))
-        .arg(Arg::new("clear")
-            .long("clear")
-            .num_args(0..)
-            .help("Clear cache"))
-        .arg(Arg::new("")
-            .long("health_check")
-            .num_args(0..)
-            .help("Enable health checking"))
-        .arg(Arg::new("ttl")
-            .long("ttl")
-            .num_args(1..)
-            .default_value("300")
-            .help("Time for the RPC to respond before we remove it from the active queue"))
-        .arg(Arg::new("supress_rpc_check")
-            .long("supress_rpc_check")
-            .num_args(1..)
-            .default_value("false")
-            .help("Supress the checking RPC health messages"))
-        .arg(Arg::new("max_retries")
-            .long("max_retries")
-            .num_args(1..)
-            .default_value("32")
-            .help("Maximum amount of retries before we drop the current request."))
-        .arg(Arg::new("health_check_ttl")
-            .long("health_check_ttl")
-            .num_args(1..)
-            .default_value("2000")
-            .help("How often to perform the health check"))
-        .arg(Arg::new("admin")
-            .long("admin")
-            .num_args(0..)
-            .help("Enable the admin namespace"))
-        .arg(Arg::new("admin_port")
-            .long("admin_port")
-            .num_args(1..)
-            .default_value("5715")
-            .help("Port to listen to for the admin namespace"))
-        .arg(Arg::new("admin_address")
-            .long("admin_address")
-            .num_args(1..)
-            .default_value("127.0.0.1")
-            .help("Address to listen to for the admin namespace"))
-        .arg(Arg::new("readonly")
-            .long("readonly")
-            .num_args(0..)
-            .help("Make the admin namespace be readonly"))
-        .arg(Arg::new("jwt")
-            .long("jwt")
-            .requires("token")
-            .num_args(0..)
-            .help("Enable admin comms with JWT"))
-        .arg(Arg::new("token")
-            .long("token")
-            .num_args(1..)
-            .requires("jwt")
-            .help("JWT token"))
+    Blutgang::command()
 }
